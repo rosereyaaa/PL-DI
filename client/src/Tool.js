4a205f83158962672f981fe126e6e70bcb156d01
Rosérey <rhyearaven@gmail.com>
2023-01-26 10:33:10 +0800

    client

--- client/src/Tool.js
+++ client/src/Tool.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Nav from "./Nav";
 
 function Tool({ name, tool }) {
     console.log(name, tool)
@@ -6,6 +7,7 @@ function Tool({ name, tool }) {
     //const tool = props.tool;
     return (
         <div>
+            <Nav />
             <h1>My name is {name}.</h1>
             <p>My favorite design tool is {tool}.</p>
         </div>
