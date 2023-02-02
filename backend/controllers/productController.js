f8c859762e504360b05279b7b6de29d9a484dd1a
Rosérey <rhyearaven@gmail.com>
2023-02-02 11:24:02 +0800

    new

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -34,3 +34,36 @@ exports.getSingleProduct = async (req, res, next) => {
     })
 }
 
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+    product = await Product.findByIdAndUpdate(req.param.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindandModify: false
+    })
+    res.send(200).json({
+        success: true,
+        product
+    })
+}
+
+exports.deleteProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+    await product.remove();
+    res.status(200).json({
+        success: true,
+        message: 'Product deleted'
+    })
+}
