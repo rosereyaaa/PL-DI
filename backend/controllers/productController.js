16476164e736895ef4a7d9ac2729d836b7c34738
Rosérey <rhyearaven@gmail.com>
2023-02-02 11:09:32 +0800

    new

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,9 @@ const Product = require('../models/product')
 exports.newProduct = async (req, res, next) => {
     console.log(req.body);
     const product = await Product.create(req.body);
+    const test = await console.log('from async');
     console.log(product);
+    console.log(test)
     res.status(201).json({
         success: true,
         product
@@ -17,3 +19,18 @@ exports.getProducts = async (req, res, next) => {
         products
     })
 }
+
+exports.getSingleProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
