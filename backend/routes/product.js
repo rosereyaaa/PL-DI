a45f1e51d06d1f0be8f5762f758f1419f9811bc4
Rosérey <rhyearaven@gmail.com>
2023-02-14 11:57:44 +0800

    new pero di pa gumagana

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,8 +4,8 @@ const { isAuthenticatedUser,
     authorizeRoles
 } = require('../middlewares/auth');
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController');
-// router.route('/products').get(getProducts);
-router.get('/products', isAuthenticatedUser, authorizeRoles('admin', 'encoder'), getProducts)
+router.get('/products', getProducts);
+// router.get('/products', isAuthenticatedUser, authorizeRoles('admin', 'encoder'), getProducts)
 router.route('/product/new').post(newProduct);
 router.route('/product/:id').get(getSingleProduct);
 router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct);
